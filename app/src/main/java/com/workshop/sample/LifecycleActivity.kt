6349e3efa9b156e059b39ee87fec341ea904d2ec
Reza Moallemi <rm.allmails@gmail.com>
2019-02-28 15:51:17 +0330

    Sace views state with onSaveInstanceState

--- app/src/main/java/com/workshop/sample/LifecycleActivity.kt
+++ app/src/main/java/com/workshop/sample/LifecycleActivity.kt
@@ -1,15 +1,53 @@
 package com.workshop.sample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.TextView
 
 class LifecycleActivity : AppCompatActivity() {
+
+    private var editText: EditText? = null
+    private var statusTextView: TextView? = null
+    private var checkbox: CheckBox? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lifecycle)
 
-        println("onCreate")
+        print("onCreate: ")
+
+        println(savedInstanceState)
+
+        editText = findViewById(R.id.editMessage)
+        statusTextView = findViewById(R.id.textStatus)
+        checkbox = findViewById(R.id.checkBox)
+
+        findViewById<View>(R.id.submitButton).setOnClickListener {
+            statusTextView?.text = editText?.text
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+
+        outState?.putString(KEY_STATUS_TEXT, statusTextView?.text.toString())
+        outState?.putBoolean(KEY_CHECKBOX_CHEECKED, checkbox?.isChecked == true)
+
+        println("onSaveInstanceState")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        println(savedInstanceState)
+
+        statusTextView?.text = savedInstanceState?.getString(KEY_STATUS_TEXT)
+        checkbox?.isChecked = savedInstanceState?.getBoolean(KEY_CHECKBOX_CHEECKED) == true
+
+        println("onRestoreInstanceState")
     }
 
     override fun onStart() {
@@ -40,5 +78,10 @@ class LifecycleActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         println("onDestroy")
+    }
+
+    companion object {
+        const val KEY_STATUS_TEXT = "status_text"
+        const val KEY_CHECKBOX_CHEECKED = "checkbox_checked"
     }
 }
